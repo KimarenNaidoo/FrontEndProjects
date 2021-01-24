defebaff0b0f945595c05d8c18194db5a6383251
KimarenNaidoo <68677767+KimarenNaidoo@users.noreply.github.com>
2021-01-24 14:18:44 +0200

    Second commit

---
+++ FaqAccordionCardMain/js/test.js
@@ -0,0 +1,88 @@
+
+
+function showDescription1() {
+    let hidden = document.getElementById("description1");
+    let image = document.getElementById("arrow1");
+    let title = document.querySelector(".title");
+    if(hidden.style.display == "block"){
+        image.style.transform = "scaleY(1)";
+        hidden.style.display = "none";
+        title.classList.remove("show");
+        title.classList.add("hide");
+
+    }else{
+        image.style.transform = "scaleY(-1)";
+        hidden.style.display = "block";
+        title.classList.remove("hide");
+        title.classList.add("show");
+    }
+    
+}
+
+function showDescription2() {
+    let hidden = document.getElementById("description2");
+    let image = document.getElementById("arrow2");
+    let title = document.querySelector(".title2");
+    if(hidden.style.display == "block"){
+        image.style.transform = "scaleY(1)";
+        hidden.style.display = "none";
+        title.classList.remove("show");
+        title.classList.add("hide");
+    }else{
+        image.style.transform = "scaleY(-1)";
+        hidden.style.display = "block";
+        title.classList.remove("hide");
+        title.classList.add("show");
+    }
+}
+
+function showDescription3() {
+    let hidden = document.getElementById("description3");
+    let image = document.getElementById("arrow3");
+    let title = document.querySelector(".title3");
+    if(hidden.style.display == "block"){
+        image.style.transform = "scaleY(1)";
+        hidden.style.display = "none";
+        title.classList.remove("show");
+        title.classList.add("hide");
+    }else{
+        image.style.transform = "scaleY(-1)";
+        hidden.style.display = "block";
+        title.classList.remove("hide");
+        title.classList.add("show");
+    }
+}
+
+function showDescription4() {
+    let hidden = document.getElementById("description4");
+    let image = document.getElementById("arrow4");
+    let title = document.querySelector(".title4");
+    if(hidden.style.display == "block"){
+        image.style.transform = "scaleY(1)";
+        hidden.style.display = "none";
+        title.classList.remove("show");
+        title.classList.add("hide");
+    }else{
+        image.style.transform = "scaleY(-1)";
+        hidden.style.display = "block";
+        title.classList.remove("hide");
+        title.classList.add("show");
+    }
+}
+
+function showDescription5() {
+    let hidden = document.getElementById("description5");
+    let image = document.getElementById("arrow5");
+    let title = document.querySelector(".title5");
+    if(hidden.style.display == "block"){
+        image.style.transform = "scaleY(1)";
+        hidden.style.display = "none";
+        title.classList.remove("show");
+        title.classList.add("hide");
+    }else{
+        image.style.transform = "scaleY(-1)";
+        hidden.style.display = "block";
+        title.classList.remove("hide");
+        title.classList.add("show");
+    }
+}
